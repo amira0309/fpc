6d5fc10d18ccd65b77341d9c43aa998b9441362c
Amira <137771893+amira0309@users.noreply.github.com>
2024-10-17 01:34:18 +0600

    секции и стили к нему

--- script.js
+++ script.js
@@ -21,3 +21,19 @@ showSlide(currentIndex);
 // Автоматическая прокрутка слайдов каждые 5 секунд
 setInterval(nextSlide, 5000);
 
+
+// Загружаем содержимое хедера из файла header.html
+fetch('header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('header-placeholder').innerHTML = data;
+    });
+
+    // Загружаем содержимое хедера из файла footer.html
+fetch('footer.html')
+.then(response => response.text())
+.then(data => {
+    document.getElementById('footer-placeholder').innerHTML = data;
+});
+
+
